baaec8638b91f5cbcd98ca46956dc038aaffac39
MY4F <67027767+MY4F@users.noreply.github.com>
2021-08-06 05:29:15 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -182,21 +182,13 @@ router.post('/linksUpdate', (req, res) => {
 const multer = require('multer');
 const fileStorageEngine = multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,'/app/views/index.html')
+        cb(null,"/app/public/images")
     },
     filename:(req,file,cb)=>{
         cb(null,file.originalname);
     }
 })
 
-const  upload = multer({storage : fileStorageEngine});
-router.post('/single',upload.single("image"),(req,res)=>{
-    req.user.update({ image1: req.file.filename }, (error, res) => {
-        if (error) throw error;
-
-    })
-    res.redirect('/dashboard');
-})
 
 
 
