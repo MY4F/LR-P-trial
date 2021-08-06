c793f226ae17d812cf82c0173fbe4f0c659e18cf
MY4F <67027767+MY4F@users.noreply.github.com>
2021-08-06 20:14:29 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -183,7 +183,7 @@ const multer = require('multer');
 const  upload = multer({storage : fileStorageEngine});
 const fileStorageEngine = multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,path.join(__dirname, '/public/images/'))
+        cb(null,path.join(__dirname, '/public/images'))
     },
     filename:(req,file,cb)=>{
         cb(null,file.originalname);
