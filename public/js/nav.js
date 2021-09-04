95d482a05f49327d2fa003e411816a5447c9726d
MY4F <67027767+MY4F@users.noreply.github.com>
2021-09-04 23:23:43 +0200

    nav error

--- public/js/nav.js
+++ public/js/nav.js
@@ -8,11 +8,11 @@ window.onscroll = function() {
     document.querySelector('.container').style.top = "0";
   } else if(list.style.display==='flex'  && window.innerWidth<751){
     document.querySelector('.container').style.top = "-800px";
-    list.style.top = "-550px";
+    list.style.top = "-560px";
   }
   else{
     document.querySelector('.container').style.top = "-160px";
-    list.style.top = "-550px";
+    list.style.top = "-560px";
   }
   prevScrollpos = currentScrollPos;
 }
