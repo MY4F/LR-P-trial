262a418f26afb55eb5939681dd26ed2e9dacdd26
MY4F <67027767+MY4F@users.noreply.github.com>
2021-09-28 17:18:48 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -134,7 +134,7 @@ router.get('/MohamedElMalatawy', (req, res) => {
 //Mohamed Yasser's page
 router.get('/MohamedYasser', (req, res) => {
     User.findOne({ name: 'Mohamed Yasser' }).then(user => {
-        res.render(dir +'/views/ClientProfilePre.ejs', {
+        res.render(dir +'/views/ClientProfile.ejs', {
           bio: user.bio,
              icons: user.icons,
              links: user.links,
