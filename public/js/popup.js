61317bb1de61a6a1378052cbba0d501d88582cae
MY4F <67027767+MY4F@users.noreply.github.com>
2021-10-11 13:50:36 +0200

    Update popup.js

--- public/js/popup.js
+++ public/js/popup.js
@@ -2,11 +2,13 @@ const purchaseBut=document.querySelectorAll('.package-request');
 const popupPurchase= document.querySelector('.popup-purchase');
 const closeSpan=document.querySelector('.close');
 const img = document.querySelectorAll('.pop-img');
+const orderType= document.querySelector('.pack-type-order');
 purchaseBut[0].addEventListener('click',()=>{
   if(!rdStandard[0].checked && !rdStandard[1].checked && !rdStandard[2].checked && !rdStandard[3].checked){
     alert("Choose the quantity you desire to reveal the price, and to be able to order.");
     return;
   }
+  orderType.textContent='Standard Package';
   img[0].src='/images/standard-card2.jpg';
   img[1].src='/images/standard-back-card.jpg';
   popupPurchase.style.display='flex';
@@ -18,6 +20,7 @@ purchaseBut[1].addEventListener('click',()=>{
     alert("Choose the quantity you desire to reveal the price, and to be able to order.");
     return;
   }
+  orderType.textContent='Premium Package';
   img[0].src='/images/prem-card2.jpg';
   img[1].src='/images/prem-card-back.jpg';
   popupPurchase.style.display='flex';
@@ -29,6 +32,7 @@ purchaseBut[2].addEventListener('click',()=>{
     alert("Choose the quantity you desire to reveal the price, and to be able to order.");
     return;
   }
+  orderType.textContent='Elite Package';
   img[0].src='/images/elite-card2.jpg';
   img[1].src='/images/elite-card-back.jpg';
   popupPurchase.style.display='flex';
