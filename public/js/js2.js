a5fb6b2bcbeddf45c5d276e6e78d9e2676a86a62
MY4F <67027767+MY4F@users.noreply.github.com>
2021-10-13 03:06:11 +0200

    Update js2.js

--- public/js/js2.js
+++ public/js/js2.js
@@ -15,6 +15,7 @@ const premiumPrice = document.querySelectorAll('.premiumPrice');
 const rdElite = document.querySelectorAll('.rdElite');
 const elitePrice = document.querySelectorAll('.elitePrice');
 const requestedText=document.querySelector('.requested-type');
+const quantityInput = document.querySelectorAll('.order-quality-input');
 const options = {
   root:null,
   threshold:0,
@@ -73,60 +74,79 @@ listItems[2].addEventListener('click',()=>{
   }
 });
 rdStandard[0].addEventListener('click',()=>{
-  standardPrice[0].childNodes[0].textContent='200 EGP ';
+  standardPrice[0].childNodes[0].textContent='170 EGP ';
   standardPrice[0].childNodes[1].textContent='/card';
-  requestedText.value='1-5';
+  requestedText.value='1-10';
+  quantityInput[0].placeholder="How many cards you need: 1-10";
+  console.log(quantityInput.placeholder);
 })
 rdStandard[1].addEventListener('click',()=>{
-  standardPrice[0].childNodes[0].textContent='160 EGP ';
-  requestedText.value='6-15';
+  standardPrice[0].childNodes[0].textContent='155 EGP ';
+  standardPrice[0].childNodes[1].textContent='/card';
+  quantityInput[0].placeholder="How many cards you need: 11-20";
+  requestedText.value='11-20';
 })
 rdStandard[2].addEventListener('click',()=>{
-  standardPrice[0].childNodes[0].textContent='120 EGP ';
-  requestedText.value='16-25';
+  standardPrice[0].childNodes[0].textContent='140 EGP ';
+  standardPrice[0].childNodes[1].textContent='/card';
+  quantityInput[0].placeholder="How many cards you need: 21-30";
+  requestedText.value='21-30';
 })
 rdStandard[3].addEventListener('click',()=>{
   standardPrice[0].childNodes[0].textContent='Custom price';
   standardPrice[0].childNodes[1].textContent='';
-  requestedText.value='25+';
+  quantityInput[0].placeholder="How many cards you need: 31+";
+  requestedText.value='31+';
 })
 
 rdPremium[0].addEventListener('click',()=>{
-  premiumPrice[0].childNodes[0].textContent='250 EGP ';
+  premiumPrice[0].childNodes[0].textContent='220 EGP ';
   premiumPrice[0].childNodes[1].textContent='/card';
-  requestedText.value='1-5';
+  quantityInput[0].placeholder="How many cards you need: 1-10";
+  requestedText.value='1-10';
 })
 rdPremium[1].addEventListener('click',()=>{
-  premiumPrice[0].childNodes[0].textContent='210 EGP ';
-  requestedText.value='6-15';
+  premiumPrice[0].childNodes[0].textContent='200 EGP ';
+  premiumPrice[0].childNodes[1].textContent='/card';
+  quantityInput[0].placeholder="How many cards you need: 11-20";
+  requestedText.value='11-20';
 })
 rdPremium[2].addEventListener('click',()=>{
-  premiumPrice[0].childNodes[0].textContent='170 EGP ';
-  requestedText.value='16-25';
+  premiumPrice[0].childNodes[0].textContent='180 EGP ';
+  premiumPrice[0].childNodes[1].textContent='/card';
+  quantityInput[0].placeholder="How many cards you need: 21-30";
+  requestedText.value='21-30';
 })
 rdPremium[3].addEventListener('click',()=>{
   premiumPrice[0].childNodes[0].textContent='Custom price';
   premiumPrice[0].childNodes[1].textContent='';
-  requestedText.value='25+';
+  quantityInput[0].placeholder="How many cards you need: 31+";
+  requestedText.value='31+';
 })
 
 rdElite[0].addEventListener('click',()=>{
-  elitePrice[0].childNodes[0].textContent='300 EGP ';
+  elitePrice[0].childNodes[0].textContent='350 EGP ';
   elitePrice[0].childNodes[1].textContent='/card';
-  requestedText.value='1-5';
+  quantityInput[0].placeholder="How many cards you need: 1-10";
+  requestedText.value='1-10';
 })
 rdElite[1].addEventListener('click',()=>{
-  elitePrice[0].childNodes[0].textContent='250 EGP ';
-  requestedText.value='6-15';
+  elitePrice[0].childNodes[0].textContent='300 EGP ';
+  elitePrice[0].childNodes[1].textContent='/card';
+  quantityInput[0].placeholder="How many cards you need: 11-20";
+  requestedText.value='11-20';
 })
 rdElite[2].addEventListener('click',()=>{
-  elitePrice[0].childNodes[0].textContent='200 EGP ';
-  requestedText.value='16-25';
+  elitePrice[0].childNodes[0].textContent='250 EGP ';
+  elitePrice[0].childNodes[1].textContent='/card';
+  quantityInput[0].placeholder="How many cards you need: 21-30";
+  requestedText.value='21-30';
 })
 rdElite[3].addEventListener('click',()=>{
   elitePrice[0].childNodes[0].textContent='Custom price';
   elitePrice[0].childNodes[1].textContent='';
-  requestedText.value='25+';
+  quantityInput[0].placeholder="How many cards you need: 31+";
+  requestedText.value='31+';
 })
 if(window.innerWidth<751){
   imgClick[1].addEventListener('click',()=>{
