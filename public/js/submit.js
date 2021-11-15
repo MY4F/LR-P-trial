e538a181ed104f82490884726e0b5e3b5105017d
MY4F <67027767+MY4F@users.noreply.github.com>
2021-11-15 11:23:29 +0200

    Submit required slots

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,3 +1,6 @@
+const check = document.querySelectorAll('.check');
 const submitBut= document.querySelector('.form-submit');
 submitBut.addEventListener('click',()=>{
-  alert("We recieved your message, hang tight till we reply back.");});
+  if(check[0].value.length>0 && check[1].value.length>0 && check[2].value.length>0 && check[3].value.length>0)
+    alert("We recieved your message, hang tight till we reply back.");
+});
