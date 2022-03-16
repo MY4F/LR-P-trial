7507015c1536e6cf19b15487b8bee8240fdbef37
MY4F <67027767+MY4F@users.noreply.github.com>
2022-03-16 16:54:50 +0200

    New promo code added and order strategy

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,21 @@ const nodemailer = require('nodemailer');
 const User = require('../models/User');
 let dir = __dirname.replace('routes','');
 const { ensureAuthenticated } = require('../config/auth');
+const multer = require("multer");
+const fs = require('fs');
+const path = require('path');
+
+const storage  = multer.diskStorage({
+    destination:(req,file,cb) =>{
+        cb(null,__dirname);
+    },
+    filename:(req,file,cb)=>{
+        //console.log(file);
+        cb(null,Date.now() + path.extname(file.originalname));
+    }
+})
+const upload = multer({storage:storage})
+
 
 let mail = nodemailer.createTransport({
     service:'gmail',
@@ -74,16 +89,26 @@ router.get('/forget', (req, res) => {
 
 //orders emails
 router.post('/packEmails',(req,res)=>{
-    let mailOptions = {
-        from:'cardtapcommunity@gmail.com',
-        to:'momoteka6089@gmail.com',
-        subject:'Order',
-        text:`Sir name: ${req.body.name},
-          email : ${req.body.email},
-          type of order :${req.body.message},
-          Phone number: ${req.body.number},
-          Quantity : ${req.body.quantity}`
-    }
+  let mailOptions = {
+      from:'cardtapcommunity@gmail.com',
+      to:'momoteka6089@gmail.com',
+      subject:'Order',
+      text:`Sir name: ${req.body.name},
+        email : ${req.body.email},
+
+        type of order :${req.body.message},
+        Phone number: ${req.body.number},
+        Quantity : ${req.body.quantity},
+        Shipping Address: ${req.body.Address},
+        Name in card : ${req.body.nameInCard},
+        Job in card : ${req.body.JobInCard}`,
+      attachments:[
+          {
+              filename: 'logo.jpg',
+              path: req.file.path
+          }
+      ]
+  }
     mail.sendMail(mailOptions,(error,info)=>{
         if(error){
             console.log(error);
