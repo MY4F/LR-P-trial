a4bd5743ada90dae2e246a41cf3ff1fd04a42c48
MY4F <67027767+MY4F@users.noreply.github.com>
2022-03-18 02:30:39 +0200

    Revert "Promo code added"
    This reverts commit 3be68919fdfc415ba77a197e274d309e2ff8c38f.

--- public/js/js2.js
+++ public/js/js2.js
@@ -16,17 +16,6 @@ const rdElite = document.querySelectorAll('.rdElite');
 const elitePrice = document.querySelectorAll('.elitePrice');
 const requestedText=document.querySelector('.requested-type');
 const quantityInput = document.querySelectorAll('.order-quality-input');
-+const promo = document.querySelector('.promo-validation');
-promo.addEventListener('click',()=>{
-  let code = document.querySelector('.code').value;
-  if(code.toLowerCase()==="tedxmfis20"){
-    promo.style.backgroundColor="#99ff99";
-    promo.innerText="Applied";
-  }
-  else{
-    promo.innerText="Denied";
-  }
-})
 const options = {
   root:null,
   threshold:0,
